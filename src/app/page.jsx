540c2e5b292f8c03e90f82b848e548aede50907c
Tristan Vidal <tres@duck.com>
2024-03-12 03:41:10 +0000

    Remove TypeScript 🧨

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+export default function Home () {
+  return (
+    <main className='flex flex-col items-center justify-between min-h-screen p-24'>
+      <div class='space-y-2'><h1 class='scroll-m-20 text-4xl font-bold tracking-tight'>Teameval</h1>
+        <p class='text-lg text-muted-foreground text-slate-500'>
+          <span>A comprehensive web platform facilitating peer evaluation of individual performance within educational team contexts.</span>
+        </p>
+      </div>
+    </main>
+  )
+}
