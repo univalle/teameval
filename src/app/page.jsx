ddf2ac7fa334bcf45b0b9c83812c5ace4496267f
Tristan Vidal <tres@duck.com>
2024-03-17 04:02:48 +0000

    Refactor class to className in page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 export default function Home () {
   return (
     <header className='px-4 py-16'>
-      <div class='space-y-2'><h1 class='scroll-m-20 text-4xl font-bold tracking-tight'>Teameval</h1>
-        <p class='text-lg text-muted-foreground text-slate-500'>
+      <div className='space-y-2'><h1 className='text-4xl font-bold tracking-tight scroll-m-20'>Teameval</h1>
+        <p className='text-lg text-muted-foreground text-slate-500'>
           <span>A comprehensive web platform facilitating peer evaluation of individual performance within educational team contexts.</span>
         </p>
       </div>
