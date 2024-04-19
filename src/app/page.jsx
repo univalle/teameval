5ccaa5148fb79d732baa6a14d875a685ccf4acf7
Tristan <tres@duck.com>
2024-04-19 12:55:16 -0500

    Revert "Update styles"
    This reverts commit c29bf25635258777441db81ec4767a69fb1f1b2e.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,7 @@
 export default function Home () {
   return (
     <header className='px-4 py-16'>
-      <div className='space-y-2'>
-        <h1 className='text-4xl font-bold tracking-tight scroll-m-20'>
-          Teameval
-        </h1>
+      <div className='space-y-2'><h1 className='text-4xl font-bold tracking-tight scroll-m-20'>Teameval</h1>
         <p className='text-lg text-muted-foreground text-slate-500'>
           <span>Apliación</span>
         </p>
