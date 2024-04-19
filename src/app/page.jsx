bd9406446c56b3aed89e46db4b5354527ffd045b
Tristan <tres@duck.com>
2024-04-19 12:27:59 -0500

    Diseño preliminar de login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ export default function Home () {
     <header className='px-4 py-16'>
       <div className='space-y-2'><h1 className='text-4xl font-bold tracking-tight scroll-m-20'>Teameval</h1>
         <p className='text-lg text-muted-foreground text-slate-500'>
-          <span>Apliación Cambio Echo por Ronald  </span>
+          <span>Apliación</span>
         </p>
       </div>
     </header>
