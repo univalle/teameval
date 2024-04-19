c29bf25635258777441db81ec4767a69fb1f1b2e
Tristan <tres@duck.com>
2024-04-19 12:32:19 -0500

    Update styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,10 @@
 export default function Home () {
   return (
     <header className='px-4 py-16'>
-      <div className='space-y-2'><h1 className='text-4xl font-bold tracking-tight scroll-m-20'>Teameval</h1>
+      <div className='space-y-2'>
+        <h1 className='text-4xl font-bold tracking-tight scroll-m-20'>
+          Teameval
+        </h1>
         <p className='text-lg text-muted-foreground text-slate-500'>
           <span>Apliación</span>
         </p>
